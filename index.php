b44deec4f156ce08b0fb5016c7d2fa2c1869d27c
Jamie <69422343+jsm8989@users.noreply.github.com>
2021-09-24 13:01:38 +0100

    Update and rename index.html to index.php

---
+++ index.php
@@ -0,0 +1,20 @@
+<html>
+   <head>
+     <meta charset="UTF-8"> 
+     <title> Collaborative page </title>
+     <link rel="stylesheet" href="../my_interest.css">
+     <script src="../my_interest.js"></script>
+                                                   
+  </head>
+  <body id="body" class="bodyorange">
+    <div class="topdiv">
+      <h1>Collaborative web page</h1>
+      <p>Head to <a href="https://github.com/jsm8989/hosted"></a> to make pull requests for this page!</p>  
+    </div>
+
+         
+    <div>
+      <p>Might be worth telling me, so I can pull from github into the web server :) </p>
+    </div>
+  </body>
+</html>

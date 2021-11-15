9264e89192b250d14152800d952786b2da68fd1e
Kieran Dalton <42457937+kierandalton@users.noreply.github.com>
2021-11-15 18:37:40 +0000

    Update index.php

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
       <p>Head <a href="https://github.com/jsm8989/hosted">here</a> to make pull requests for this page!</p>  
     </div>
 
+    <?php include('WalesVsWhales.html'); ?>
          
     <div>
       <p>Might be worth telling me, so I can pull from github into the web server :) </p>
